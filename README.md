a419a7fea3688afbc65387bde99102dcab1211e6
amaranabe <amaiamarana@gmail.com>
2015-05-27 00:28:26 +0200

    funciones

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Phonegap
+Este repositorio alberga diversos proyectos desarrollados con Phonegap
+
+##* Primerraapp: aplicación que gestiona costes puntuales
+##* Gestsoci: 
+		*Función: aplicación que gestiona los consumos que se realizan en una sociedad
+		*Elementos:
+				-Usuarios
+				-Elección productos
+				-Generación factura --> cálculo costes
+		*Herramientas:
+				-Phonegap
+				-HTML5, JQUERY MOBILE, JAVASCRIPT, CSS
+				-SQLlite, base de datos que android tiene por defecto. 
--- gestsoci/www/js/myindex.js
+++ gestsoci/www/js/myindex.js
@@ -1,78 +1,33 @@
 
 //VARIABLES
-	var nombre_socio = "";
-	var total_sociedad = 0;
-	var total_produtos = 0;
-	var total_factura = 0;
-	var turno = "";
+	var socio_logeado = "";  // guardaremos el nombre del socio que se ha loggeado
+	var total_sociedad = 0;  // coste de los elementos cocina utilizados (servilletas, mesas,...)
+	var total_productos = 0;  // coste de los productos consumidos
+	var total_factura = 0; // coste total de la factura
+	
+	var turno = ""; 
 	var num_comensales = 0;
-	var strMesas= "";
-
-	//var mesas_cogidas[12]= {false,false,,false,false,false,false,false,false,false,false,false,false};
+	var strMesas= ""; //guardamos en un string las mesas
 		
 //CONEXIÓN A BASE DE DATOS
 
-/* Funcion para saber un id disponible de una tabla*/
+/* Funcion para mostrar una tabla. De momento sólo hay una.*/
+function showTable(tableName) {
+	var str = "" ;
+	str="MOSTRANDO LA TABLA "+tableName+":\n";
+    var db = window.openDatabase("gestionsocios", "1.0", "Gestion Soci", 200000);    
+    db.transaction(function (tx) { 							
+	                tx.executeSql('SELECT * FROM ' + tableName, [], function (tx, results) {						 	
+						 	var len = results.rows.length;
+						 	//alert ("number of results : " + len);
+							for (var i=0; i<len; i++){
+								var colname = results.rows.item(i).Name;
+								str = str + "Row = " + i + " id_socio:" + results.rows.item(i).id_socio + " nombre_socio:" + results.rows.item(i).nombre_socio + "\n";
+							}
+							alert(str);
+					},null);
 
-function nextIdInTableAvailable(id_name, tableName)
-{
-
-
-    var db = window.openDatabase("gestionsocios", "1.0", "Gestion Soci", 200000);
-
-    
-    db.transaction(function (tx)
-    				{
-
-		              /*  tx.executeSql('SELECT '+ id_name +' FROM ' + tableName + ' ORDER BY '+id_name+' ASC', [], 
-		                	function (tx, results) {
-        			        			 	var str = "" ;
-										 	var len = results.rows.length;
-										 	alert ("number of results : " + len);
-										 	var max_id=0;
-											for (var i=0; i<len; i++){
-												str = str + "Row = " + i + " ID = " + eval('results.rows.item(i).'+id_name) + "\n";
-												if (max_id <eval('results.rows.item(i).'+id_name)
-													max_id = eval('results.rows.item(i).'+id_name);
-											}
-											alert(str);
-
-											console.log("hay resultados en la vuelta " + max_id);
-
-										},null);*/
-
-        			}, null, null);
-
-}
-
-/* Funcion para mostrar una tabla*/
-function showTable(tableName)
-{
-
-	alert("MOSTRANDO LA TABLA "+tableName);
-
-    var db = window.openDatabase("gestionsocios", "1.0", "Gestion Soci", 200000);
-
- 
-    
-    db.transaction(function (tx)
-    				{
-    							
-
-		                tx.executeSql('SELECT * FROM ' + tableName, [], function (tx, results) {
-		                									 	var str = "" ;
-        			    									 	var len = results.rows.length;
-        			        								 	alert ("number of results : " + len);
-		        												for (var i=0; i<len; i++){
-		        													var colname = results.rows.item(i).Name;
-		            														str = str + "Row = " + i + " id_socio:" + results.rows.item(i).id_socio + " nombre_socio:" + results.rows.item(i).nombre_socio + "\n";
-	    	    												}
-	        													alert(str);
-
-											},null);
-
-        			}, null, null);
-
+	}, null, null);
 }
 
 
@@ -89,19 +44,21 @@ function showTable(tableName)
 	        	for (var i=0; i<len; i++){
 	            	str = str + "Row = " + i + " ID = " + results.rows.item(i).id_socio + " Nombre =  " + results.rows.item(i).nombre_socio + " Pass = " + results.rows.item(i).pass_socio + "\n";
 	        	}
-	        	alert(str);
 
 				var encontrado_pass=false;
 	        	for (var i = 0; i < results.rows.length; i++) {
 	        		if (results.rows.item(i).pass_socio== document.getElementById('txtclave').value) {
 	        				//password existe --> accedemos al menú de la aplicación
 	        				encontrado_pass=true;
+	        				socio_logeado = results.rows.item(i).nombre_socio;
+	        				alert("Bienvenido usuario " + socio_logeado);
 	        		}
         		}
-
-        		if(encontrado_pass) window.open('#menu','_self');
-        		else {alert('Password incorrecto');}
-        		
+        		if(encontrado_pass) {
+        			//self, abre un link en la misma ventana
+					 window.open('#menu','_self');
+        		}
+        		else {alert('Password incorrecto');}      		
         	}
     }
 
@@ -115,85 +72,49 @@ function showTable(tableName)
 
 	function login(){
 		//abro la base de datos
-		var db = window.openDatabase("gestionsocios", "1.0", "Gestion Soci", 3);
+		var db = window.openDatabase("gestionsocios", "1.0", "Gestion Soci", 200000);
 	    db.transaction(logincheck, errorBD, successTransaction);
 	}
 
 	// Create and populate the database 
     function logincheck(tx) {
-       
         nombre_socio = document.getElementById("txtsociologin").value;
         tx.executeSql('SELECT * FROM socios WHERE nombre_socio="' + nombre_socio +'"', [], selectSuccess, errorBD);
     }
 
 
+//FUNCIONES PARA CALCUALR EL COSTE DE LOS CONSUMOS
 
-/* Funciones para calcular la factura*/
+/* Funciones para calcular la factura, que serán llamados cuando generemos factura*/
 	function totalSociedad(){
-		
 		num_comensales = document.getElementById("comensales").value;
-		
 		//mirar que turno se ha seleccionado
 		 var temp=document.getElementsByName("turnocomida");
-        // Recorremos todos los valores del radio button para encontrar el
-        // seleccionado
-        for(var i=0;i<temp.length;i++)
-        {
+        // Recorremos todos los valores del radio button para encontrar el valor seleccionado
+        for(var i=0;i<temp.length;i++) {
             if(temp[i].checked)
                 turno = temp[i].value;
         }
 		
 		//mirar que mesas se han reservado
-
-		if (document.getElementById("mesa1").checked){
-			strMesas = strMesas + " 1 -"
+		strMesas="";
+		for (var i=1;i<=12;i++){
+			if ( document.getElementById('mesa'+i).checked ){
+					if (strMesas.length > 0) strMesas = strMesas + " - "; 
+					strMesas = strMesas + i;
+			}
 		}
-		if (document.getElementById("mesa2").checked){
-			strMesas = strMesas + " 2 -"
-		}
-		if (document.getElementById("mesa3").checked){
-			strMesas = strMesas + " 3 -"
-		}
-		if (document.getElementById("mesa4").checked){
-			strMesas = strMesas + "4 -"
-		}
-		if (document.getElementById("mesa5").checked){
-			strMesas = strMesas + " 5 -"
-		}
-		if (document.getElementById("mesa6").checked){
-			strMesas = strMesas + "6 -"
-		}
-		if (document.getElementById("mesa7").checked){
-			strMesas = strMesas + " 7 -"
-		}
-		if (document.getElementById("mesa8").checked){
-			strMesas = strMesas + " 8 -"
-		}
-		if (document.getElementById("mesa9").checked){
-			strMesas = strMesas + " 9 -"
-		}
-		if (document.getElementById("mesa10").checked){
-			strMesas = strMesas + " 10 -"
-		}
-		if (document.getElementById("mesa11").checked){
-			strMesas = strMesas + " 11 -"
-		}
-		if (document.getElementById("mesa12").checked){
-			strMesas = strMesas + " 12"
-		}
-
 
 		total_sociedad=(1.50*document.getElementById("mantel").value)+(5.50*document.getElementById("aceitelitro").value)+
 		(0.80*document.getElementById("aceitebotellin").value)+(1.50*document.getElementById("descorcheaceite").value)+
 		(1.50*document.getElementById("patatasfritas").value)+(0.25*document.getElementById("servilleta").value)+
 		(0.40*document.getElementById("gas").value)+(0.40*document.getElementById("asadorelectrico").value)+
-		(0.80*document.getElementById("despensasocio").value)+(1.50*document.getElementById("despensanosocio").value);
-
-		
+		(0.80*document.getElementById("despensasocio").value)+(1.50*document.getElementById("despensanosocio").value);	
 	}
 
+
 	function totalproductos(){
-		total_produtos=(4.00*document.getElementById("vinocosechero").value)+(8.00*document.getElementById("vinocrianza34").value)+
+		total_productos=(4.00*document.getElementById("vinocosechero").value)+(8.00*document.getElementById("vinocrianza34").value)+
 		(16.00*document.getElementById("vinocrianzamagnum").value)+(11.00*document.getElementById("vinoreserva").value)+
 		(4.50*document.getElementById("vinoclarete").value)+(4.50*document.getElementById("vinoclareteaguja").value)+
 		(5.50*document.getElementById("vinoblancoseco").value)+(5.50*document.getElementById("vinoblancoaguja").value)+
@@ -211,16 +132,88 @@ function showTable(tableName)
 		(1.00*document.getElementById("cocacola").value)+(1.40*document.getElementById("cerveza").value)+
 		(0.75*document.getElementById("infusiones").value)+(0.75*document.getElementById("cafesolocortadoconleche").value)+
 		(0.75*document.getElementById("descafeinado").value)+(1.00*document.getElementById("puros").value);
-
+		//alert("total: " + total_produtos);
 	}
 
 	function totalfactura(){
-		document.getElementById("txtsocio").innerHTML = nombre_socio;
+		totalSociedad();
+		totalproductos();
+
+		document.getElementById("txtsocio").innerHTML = socio_logeado;
 		document.getElementById("txtturno").innerHTML = turno;
 		document.getElementById("txtmesa").innerHTML = strMesas;
 		document.getElementById("txtcomensales").innerHTML = num_comensales;
-		document.getElementById("txtsociedad").innerHTML = total_sociedad;		
-		document.getElementById("txttotalconsumido").innerHTML = total_produtos;
-		document.getElementById("txttotalfactura").innerHTML = total_sociedad + total_produtos;
-		document.getElementById("txttotalporpersona").innerHTML = (total_sociedad + total_produtos) / num_comensales;
+		//utilizo método toFixed() para mostrar float con 2 decimales. Cuidado, devuelve un string (sólo usar para mostrar).
+		document.getElementById("txtsociedad").innerHTML = total_sociedad.toFixed(2);		
+		document.getElementById("txttotalconsumido").innerHTML = total_productos.toFixed(2);
+		document.getElementById("txttotalfactura").innerHTML = (total_sociedad + total_productos).toFixed(2);	
+		var total_por_persona; 
+		if (num_comensales == 0) { total_por_persona=0; }
+		else {
+			total_por_persona= (total_sociedad + total_productos) / num_comensales;
+		}
+			
+		document.getElementById("txttotalporpersona").innerHTML = total_por_persona.toFixed(2);
+	}
+
+	function cerrarSesion(){
+
+		document.getElementById("comensales").value="";
+		 var temp=document.getElementsByName("turnocomida");
+        // Recorremos todos los valores del radio button para encontrar el valor seleccionado
+        for(var i=0;i<temp.length;i++) {
+            temp[i].checked =false;
+           }
+
+		for (var i=1;i<=12;i++){
+			document.getElementById('mesa'+i).checked =false;
+		}
+		document.getElementById("mantel").value="";
+		document.getElementById("aceitelitro").value="";
+		document.getElementById("aceitebotellin").value="";
+		document.getElementById("descorcheaceite").value="";
+		document.getElementById("patatasfritas").value="";
+		document.getElementById("servilleta").value="";
+		document.getElementById("gas").value="";
+		document.getElementById("asadorelectrico").value="";
+		document.getElementById("despensasocio").value="";
+		document.getElementById("despensanosocio").value="";	
+
+	    document.getElementById("vinocosechero").value="";
+	    document.getElementById("vinocrianza34").value="";
+		document.getElementById("vinocrianzamagnum").value="";
+		document.getElementById("vinoreserva").value="";
+		document.getElementById("vinoclarete").value="";
+		document.getElementById("vinoclareteaguja").value="";
+		document.getElementById("vinoblancoseco").value="";
+		document.getElementById("vinoblancoaguja").value="";
+		document.getElementById("txakoli").value="";
+		document.getElementById("sidra").value="";
+		document.getElementById("descorchedevinos").value="";
+		document.getElementById("cava").value="";
+		document.getElementById("champagne").value="";
+		document.getElementById("descorchecavas").value="";
+		document.getElementById("coñac").value="";
+		document.getElementById("ginebra").value="";
+		document.getElementById("anis").value="";
+		document.getElementById("cointreau").value="";
+		document.getElementById("whisky").value="";
+		document.getElementById("whiskydemalta").value="";
+		document.getElementById("baileys").value="";
+		document.getElementById("ron").value="";
+		document.getElementById("ronreserva").value="";
+		document.getElementById("vodka").value="";
+		document.getElementById("licormanzanamelocoton").value="";
+		document.getElementById("licordehierbas").value="";
+		document.getElementById("armagnac").value="";
+		document.getElementById("aguamineral").value="";
+		document.getElementById("tonica").value="";
+		document.getElementById("kas").value="";
+		document.getElementById("cocacola").value="";
+		document.getElementById("cerveza").value="";
+		document.getElementById("infusiones").value="";
+		document.getElementById("cafesolocortadoconleche").value="";
+		document.getElementById("descafeinado").value="";
+		document.getElementById("puros").value="";
+
 	}
